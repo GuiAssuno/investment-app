452f0c5035a030378fe3e8854e89d3173b79bfdd
Guilherme Assunção <Gui_ab_@hotmail.com>
2026-03-18 11:20:43 -0300

    Improve comments in extracao_bruta.py
    Refine comments for clarity and consistency.

--- arq/util/extracao_bruta.py
+++ arq/util/extracao_bruta.py
@@ -10,20 +10,19 @@ driver = webdriver.Chrome(service=servico)
 try:
     print("Acessando Google Finance...")
     driver.get("https://www.google.com/finance/quote/ITUB3:BVMF")
-    time.sleep(4) # Espera um pouco mais para garantir
+    time.sleep(4)
 
-    # Pega TODO o texto da página
+    # Pega  o texto da página
     texto_pagina = driver.find_element(By.TAG_NAME, "body").text
     
-    # Divide o texto em uma lista de linhas
+    # TRANSFORMA EM UMA LISTA DE LINHAS
     linhas = texto_pagina.split('\n')
     
     print("\n--- MAPA DA PÁGINA (Raio-X) ---")
     print("Procure onde o preço aparece e anote o número da linha (Índice)\n")
     
-    # Vamos imprimir apenas as primeiras 30 linhas, onde o preço costuma ficar
+    #  imprimir as 30 primeiras  linhas
     for indice, linha in enumerate(linhas[:50]):
-        # Se a linha não estiver vazia, imprime
         if linha.strip():
             print(f"[{indice}] {linha}")
 
